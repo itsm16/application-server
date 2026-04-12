9e0066093938ef2fb5a104c3f92a3c65000f4241
itsm16 <mrinalg2k1@gmail.com>
2026-04-12 23:11:42 +0530

    add origin

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import "dotenv/config"
 const PORT = process.env.PORT || 3000
 
 const app = express()
-app.use(cors({origin: "http://localhost:5173", credentials: true}))
+app.use(cors({origin: process.env.ORIGIN, credentials: true}))
 app.use(express.json())
 app.use(cookieParser())
 
