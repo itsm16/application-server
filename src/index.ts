7737e53479139041acf56745df7c96538e18a84d
itsm16 <mrinalg2k1@gmail.com>
2026-04-13 02:16:25 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(cors({
   origin, credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
-app.options('*', cors());
+app.options('/*splat', cors()); 
 app.use(express.json())
 app.use(cookieParser())
 
